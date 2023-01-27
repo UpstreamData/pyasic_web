9c9ac4dbf2a549bf2a20a3fbb9fdc32f69a186a1
UpstreamData <brett@upstreamdata.ca>
2023-01-27 13:08:23 -0700

    Add pool users and improve look of UI.

--- pyasic_web/dashboard/ws.py
+++ pyasic_web/dashboard/ws.py
@@ -20,6 +20,18 @@ class MinerDataManager(metaclass=Singleton):
         self.cached_data = None
 
 
+def get_pool_users_data(data: list):
+    users = {}
+    pool_data = [dp.get("pool_1_user") for dp in data]
+    for user in pool_data:
+        if user:
+            if not user in users:
+                users[user] = 0
+            users[user] += 1
+    return users
+
+
+
 @router.websocket("/dashboard/ws")
 async def dashboard_websocket(websocket: WebSocket):
     await websocket.accept()
@@ -28,13 +40,17 @@ async def dashboard_websocket(websocket: WebSocket):
     graph_sleep_time = get_current_settings()["graph_data_sleep_time"]
     data_manager = MinerDataManager()
     try:
-        if data_manager.cached_data:
-            await websocket.send_json(
-                {
-                    "datetime": datetime.datetime.now().isoformat(),
-                    "miners": data_manager.cached_data,
-                }
-            )
+        miners = get_current_miner_list()
+        if len(miners) > 0:
+            if data_manager.cached_data:
+                pool_users = get_pool_users_data(data_manager.cached_data)
+                await websocket.send_json(
+                    {
+                        "datetime": datetime.datetime.now().isoformat(),
+                        "miners": data_manager.cached_data,
+                        "pool_users": pool_users,
+                    }
+                )
         while True:
             miners = get_current_miner_list()
             all_miner_data = []
@@ -46,10 +62,12 @@ async def dashboard_websocket(websocket: WebSocket):
                 all_miner_data.append(data_point)
             all_miner_data.sort(key=lambda x: ipaddress.ip_address(x["ip"]))
             data_manager.cached_data = all_miner_data
+            pool_users = get_pool_users_data(data_manager.cached_data)
             await websocket.send_json(
                 {
                     "datetime": datetime.datetime.now().isoformat(),
                     "miners": all_miner_data,
+                    "pool_users": pool_users,
                 }
             )
             await asyncio.sleep(graph_sleep_time)
