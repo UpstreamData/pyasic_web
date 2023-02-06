736f0c5c67555a3ea628fdc6070e3a41ce4efcf1
UpstreamData <brett@upstreamdata.ca>
2023-02-06 11:55:32 -0700

    Update miner page styling.

--- pyasic_web/views/miner/__init__.py
+++ pyasic_web/views/miner/__init__.py
@@ -76,7 +76,7 @@ async def ws_miner(websocket: WebSocket):
     miner_data_timeout = settings["miner_data_timeout"]
     data_manager = SingleMinerDataManager()
     if data_manager.cached_data:
-        await websocket.send_json(data_manager.cached_data)
+        await websocket.send_text(data_manager.cached_data)
     try:
         while True:
             try:
@@ -84,33 +84,18 @@ async def ws_miner(websocket: WebSocket):
                     MinerFactory().get_miner(str(miner_ip)), miner_identify_timeout
                 )
                 data = await asyncio.wait_for(cur_miner.get_data(), miner_data_timeout)
+                data = data.as_json()
 
-                fan_speeds = [
-                    fan if not fan == -1 else 0
-                    for fan in [data.fan_1, data.fan_2, data.fan_3, data.fan_4]
-                ]
-                data = {
-                    "hashrate": data.hashrate,
-                    "fans": fan_speeds,
-                    "temp": data.temperature_avg,
-                    "datetime": datetime.datetime.now().isoformat(),
-                    "model": data.model,
-                    "efficiency": data.efficiency,
-                    "wattage": data.wattage,
-                    "max_wattage": data.wattage_limit,
-                    "fault_light": data.fault_light,
-                    "errors": [err.error_message for err in data.errors],
-                }
                 data_manager.cached_data = data
-                await websocket.send_json(data)
+                await websocket.send_text(data)
                 await asyncio.sleep(settings["graph_data_sleep_time"])
             except asyncio.exceptions.TimeoutError:
-                data = {"errors": ["The miner is not responding."]}
+                data = {"py_errors": ["The miner is not responding."]}
                 await websocket.send_json(data)
                 await asyncio.sleep(0.5)
             except KeyError as e:
                 print(e)
-                data = {"errors": ["The miner returned unusable/unsupported data."]}
+                data = {"py_errors": ["The miner returned unusable/unsupported data."]}
                 await websocket.send_json(data)
                 await asyncio.sleep(0.5)
     except WebSocketDisconnect:
